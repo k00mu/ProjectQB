908d1546b8fe961fae654b1fff2a1b3dce21aed3
Atqa Munzir <atqamz@gmail.com>
2024-08-26 06:59:04 +0700

    feat: implement minimal duck feet mechanics

---
+++ Assets/_Project/Scripts/Common/RandomMovingPlatformTriggerTimer.cs
@@ -0,0 +1,33 @@
+﻿// ==================================================
+// 
+//   Created by Atqa Munzir
+// 
+// ==================================================
+
+using MoreMountains.CorgiEngine;
+using System.Collections;
+using UnityEngine;
+
+namespace QuackleBit
+{
+    public class RandomMovingPlatformTriggerTimer : MonoBehaviour
+    {
+        private MovingPlatform movingPlatform;
+        
+        private void Awake()
+        {
+            movingPlatform = GetComponent<MovingPlatform>();
+        }
+
+        private void Start()
+        {
+            StartCoroutine(TriggerMovingPlatformInRandomTime(0, 3));
+        }
+        
+        private IEnumerator TriggerMovingPlatformInRandomTime(float min, float max)
+        {
+            yield return new WaitForSeconds(Random.Range(min, max));
+            movingPlatform.AuthorizeMovement();
+        }
+    }
+}
