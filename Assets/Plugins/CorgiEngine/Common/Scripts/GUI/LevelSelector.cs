cd86f208727bfa76b3e28edc465764ec4ba3c861
Atqa Munzir <atqamz@gmail.com>
2024-08-25 22:21:52 +0700

    chore: minor scene adjustment with GDD

--- Assets/Plugins/CorgiEngine/Common/Scripts/GUI/LevelSelector.cs
+++ Assets/Plugins/CorgiEngine/Common/Scripts/GUI/LevelSelector.cs
@@ -38,6 +38,6 @@ namespace MoreMountains.CorgiEngine
 			// we trigger an unPause event for the GameManager (and potentially other classes)
 			//CorgiEngineEvent.Trigger(CorgiEngineEventTypes.UnPause);
 			//LoadingSceneManager.LoadScene(SceneManager.GetActiveScene().name);
-		}		
+		}
 	}
 }
---
+++ Assets/_Project/Scripts/Common/LevelSelector.cs
@@ -0,0 +1,10 @@
+namespace QuackleBit
+{
+    public class LevelSelector : MoreMountains.CorgiEngine.LevelSelector
+    {
+        public void QuitGame()
+        {
+            Komutils.Helpers.QuitGame();
+        }
+    }
+}
