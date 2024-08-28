a3a775cc7d185895979630482d07bca49af0c6b5
rendyrayana <rendyrayanahp@gmail.com>
2024-08-28 15:22:07 +0900

    Asset:
    Test Tilemap

---
+++ Assets/Environment/Tileset/Test Tileset/IMG_0619.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="IMG_0619" tilewidth="32" tileheight="32" tilecount="1638" columns="21">
+ <image source="IMG_0619.PNG" width="672" height="2500"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="7">
+   <object id="6" x="-0.123385" y="3.08463" width="31.9568" height="28.6254"/>
+  </objectgroup>
+ </tile>
+</tileset>
