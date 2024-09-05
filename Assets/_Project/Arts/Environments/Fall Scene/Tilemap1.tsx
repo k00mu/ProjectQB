0a1ae07c5b714bc999c8f8ff73e80f30b28717f8
Atqa Munzir <atqamz@gmail.com>
2024-09-05 23:37:46 +0700

    asset: structuring & import more required asset

---
+++ Assets/_Project/Arts/Environments/Fall Scene/Tilemap1.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Tilemap1" tilewidth="32" tileheight="32" tilecount="1470" columns="21">
+ <image source="Tilemap1.PNG" width="672" height="2240"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="3" width="16" height="29"/>
+   <object id="2" x="23" y="22"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="5">
+   <object id="4" x="0" y="3" width="32" height="29"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="3" width="16" height="29"/>
+  </objectgroup>
+ </tile>
+</tileset>
