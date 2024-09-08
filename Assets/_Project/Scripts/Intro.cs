72295cea919d02872133e6241cb2f35b8167cd4a
Atqa Munzir <atqamz@gmail.com>
2024-09-08 07:30:09 +0700

    feat: intro scene

---
+++ Assets/_Project/Scripts/Intro.cs
@@ -0,0 +1,36 @@
+﻿// ==================================================
+// 
+//   Created by Atqa Munzir
+// 
+// ==================================================
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+using WaitForSeconds = UnityEngine.WaitForSeconds;
+
+namespace QuackleBit
+{
+	public class Intro : MonoBehaviour
+	{
+		[SerializeField] private VideoPlayer _videoPlayer;
+		[SerializeField] private ScreenFade _screenFade;
+		
+		private void Start()
+		{
+			StartCoroutine(PlayIntro());
+		}
+		
+		private IEnumerator PlayIntro()
+		{
+			_screenFade.FadeIn(0.5f);
+			yield return new WaitForSeconds(0.5f);
+			_videoPlayer.Play();
+			yield return new WaitUntil(() => _videoPlayer.isPlaying == false);
+			_screenFade.FadeOut(0.5f);
+			yield return new WaitForSeconds(0.5f);
+			SceneManager.LoadScene("Loading");
+		}
+	}
+}
---
+++ Assets/_Project/Scripts/ScreenFade.cs
@@ -0,0 +1,54 @@
+﻿// ==================================================
+// 
+//   Created by Atqa Munzir
+// 
+// ==================================================
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace QuackleBit
+{
+	public class ScreenFade : MonoBehaviour
+	{
+		private Image _image;
+		
+		private void Awake()
+		{
+			_image = GetComponent<Image>();
+		}
+
+		public void FadeIn(float duration)
+		{
+			StartCoroutine(FadeInCoroutine(duration));
+		}
+		
+		public void FadeOut(float duration)
+		{
+			StartCoroutine(FadeOutCoroutine(duration));
+		}
+
+		private IEnumerator FadeInCoroutine(float duration)
+		{
+			float t = 0f;
+			while (t < 1f)
+			{
+				t += Time.deltaTime / duration;
+				_image.color = new Color(_image.color.r, _image.color.g, _image.color.b, 1f - t);
+				yield return null;
+			}
+		}
+		
+		private IEnumerator FadeOutCoroutine(float duration)
+		{
+			float t = 0f;
+			while (t < 1f)
+			{
+				t += Time.deltaTime / duration;
+				_image.color = new Color(_image.color.r, _image.color.g, _image.color.b, t);
+				yield return null;
+			}
+		}
+	}
+}
