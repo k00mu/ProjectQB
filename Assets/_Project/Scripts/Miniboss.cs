29d6266141ea0bfce3cb062f52c355f80212848a
Atqa Munzir <atqamz@gmail.com>
2024-09-10 17:10:31 +0700

    feat: miniboss asset sync

---
+++ Assets/_Project/Scripts/Miniboss.cs
@@ -0,0 +1,18 @@
+﻿// ==================================================
+// 
+//   Created by Atqa Munzir
+// 
+// ==================================================
+using UnityEngine;
+namespace QuackleBit
+{
+	public class Miniboss : MonoBehaviour
+	{
+		[SerializeField] private Animator _animator;
+		
+		public void Stall(bool stall)
+		{
+			_animator.SetBool("Stall", stall);
+		}
+	}
+}
