fec855c6e4604d7910823899088885ed3453220e
Atqa Munzir <atqamz@gmail.com>
2024-09-12 14:15:25 +0700

    fix: flyingberrybomb drop speed by direction change

--- Assets/_Project/Scripts/FlyingBerryBomb.cs
+++ Assets/_Project/Scripts/FlyingBerryBomb.cs
@@ -5,6 +5,7 @@
 // ==================================================
 
 using Komutils.Extensions;
+using MoreMountains.CorgiEngine;
 using MoreMountains.Tools;
 using System;
 using System.Collections;
@@ -14,7 +15,9 @@ namespace QuackleBit
 {
 	public class FlyingBerryBomb : MonoBehaviour
 	{
+		private Transform _model;
 		private BoxCollider2D _collider2D;
+		private ThrownObject _thrownObject;
 
 		private void Awake()
 		{
@@ -29,6 +32,10 @@ namespace QuackleBit
 
 		public void MakeOrphan()
 		{
+			_model = transform.parent.GetChild(0);
+			_thrownObject = GetComponent<ThrownObject>();
+			if (Mathf.Approximately(_thrownObject.Direction.x, _model.localScale.x))
+				_thrownObject.Speed *= -1;
 			transform.parent = null;
 		}
 
